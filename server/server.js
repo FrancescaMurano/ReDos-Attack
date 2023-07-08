ddd2daa30d2ce8193874f5172c1b330642099bda
fra <mrnfnc99d60b774e@studenti.unical.it>
2023-07-08 17:12:59 +0200

    insert also a regex injection in register post request

--- server/server.js
+++ server/server.js
@@ -10,16 +10,35 @@ app.use(cors({
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-
+// symple ReDos
 app.post('/login', (req, res) => {
   let data = req.body;
   let pattern2 = /^(([a-z])+.)+[A-Z]([a-z])+$/;
   let result2 = pattern2.test(data["username"]);
-  console.log(result2)
-
+  // console.log(result2)
   res.send(result2);
+})
+
+// Vulnerable to regex injection, attacker insert in username field an evil regex and in password a crafted input that implies ReDos.
+app.post('/register', (req, res) => {
+  let data = req.body;
+  username = data["username"]
+  password = data["password"]
+  testPassword = new RegExp(username);
+  
+  match = password.match(testPassword);
+
+  if (match !== null)
+    res.send("Password must not contain the user's username!");
+  else
+    res.send("Ok.");
 })
 
 app.listen(3200, () => {
   console.log('Example app listening on port 3200!')
 })
+
+// EVIL REGEX TO INSERT ^(([a-z])+.)+[A-Z]([a-z])+$ 
+
+// curl -X POST  http://localhost:3200/register -d "username=^(([a-z])%2B.)%2B[A-Z]([a-z])%2B$&password=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa!"
+
